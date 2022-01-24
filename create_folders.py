eb1f7be437afc8c6ae8ec392b1c4b457173348ca
Roman_Max <55191674+MaksimovRS2018@users.noreply.github.com>
2022-01-24 21:03:19 +0300

    Initial commit

---
+++ create_folders.py
@@ -0,0 +1,19 @@
+
+config = "config.txt"
+import os
+
+with open(config, "r", encoding='utf-8') as conf:
+    a = conf.readlines()
+    for i in range(len(a)):
+        try:
+            number_test = a[i].split("\n")[0]
+            path = "tests/"
+            new_folder =""
+            for k in range(len(number_test.split(".")) - 1):
+                new_folder = new_folder + number_test.split(".")[k] + "."
+                path = path + new_folder + "/"
+            path = path + "/" +  number_test
+            os.makedirs(path)
+        except FileExistsError:
+            continue
+    conf.close()
